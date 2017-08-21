8c233f32e3df8a5f83ca84d13c479227154a4269
Javier Gamarra <nhpatt@gmail.com>
2017-08-21 18:40:43 +0200

    apply liferay formatting

--- app/src/main/java/com/nhpatt/pushexample/PushReceiver.java
+++ app/src/main/java/com/nhpatt/pushexample/PushReceiver.java
@@ -3,8 +3,8 @@ package com.nhpatt.pushexample;
 import com.liferay.mobile.push.PushNotificationsReceiver;
 
 public class PushReceiver extends PushNotificationsReceiver {
-    @Override
-    public String getServiceClassName() {
-        return PushService.class.getName();
-    }
+	@Override
+	public String getServiceClassName() {
+		return PushService.class.getName();
+	}
 }
