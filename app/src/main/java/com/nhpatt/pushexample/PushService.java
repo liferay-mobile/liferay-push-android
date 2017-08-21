950caf8f9dac6b4b8bc74948e2d079b320a50a60
Javier Gamarra <nhpatt@gmail.com>
2017-08-21 10:06:22 +0200

    new example app

---
+++ app/src/main/java/com/nhpatt/pushexample/PushService.java
@@ -0,0 +1,17 @@
+package com.nhpatt.pushexample;
+
+import android.util.Log;
+
+import com.liferay.mobile.push.PushNotificationsService;
+
+import org.json.JSONObject;
+
+public class PushService extends PushNotificationsService {
+
+	@Override
+	public void onPushNotification(JSONObject jsonObject) {
+		super.onPushNotification(jsonObject);
+		Log.d("PUSH_NOTIFICATION", "Push notification received!" + jsonObject.toString());
+	}
+
+}
