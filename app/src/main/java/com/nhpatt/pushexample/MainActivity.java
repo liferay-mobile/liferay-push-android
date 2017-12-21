916ea8301052d6c0f21dae8b4ab38e6c007b788b
Javier Gamarra <nhpatt@gmail.com>
2017-12-21 13:34:24 +0100

    update to stable version

--- app/src/main/java/com/nhpatt/pushexample/MainActivity.java
+++ app/src/main/java/com/nhpatt/pushexample/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		findViewById(R.id.push_send).setOnClickListener(this);
 
 		Session session =
